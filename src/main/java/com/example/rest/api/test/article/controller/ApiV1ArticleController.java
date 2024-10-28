d9f77c41bb949933f0eee09d02b668abdc1ba873
anyeok <https://github.com/anyeok>
2024-10-28 12:10:40 +0900

    CUD 테스트 완료

--- src/main/java/com/example/rest/api/test/article/controller/ApiV1ArticleController.java
+++ src/main/java/com/example/rest/api/test/article/controller/ApiV1ArticleController.java
@@ -34,17 +34,23 @@ public class ApiV1ArticleController {
     }
 
     @PostMapping("")
-    public String create() {
-        return "등록";
+    public String create(@RequestParam("subject") String subject, @RequestParam("content") String content) {
+        System.out.println(subject);
+        System.out.println(content);
+        return "등록 완료";
     }
 
     @PatchMapping("/{id}")
-    public String modify(@PathVariable("id") Long id) {
-        return "수정";
+    public String modify(@PathVariable("id") Long id, @RequestParam("subject") String subject, @RequestParam("content") String content) {
+        System.out.println(id);
+        System.out.println(subject);
+        System.out.println(content);
+        return "수정 완료";
     }
 
     @DeleteMapping("/{id}")
     public String delete(@PathVariable("id") Long id) {
-        return "삭제";
+        System.out.println(id);
+        return "삭제 완료";
     }
 }
