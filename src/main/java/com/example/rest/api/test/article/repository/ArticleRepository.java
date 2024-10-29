290b4ab4dde43f0739c6c05a4d0b05b4153c3145
anyeok <https://github.com/anyeok>
2024-10-29 12:30:50 +0900

    14강, 게시글 다건, 단건 조회 API작업

--- src/main/java/com/example/rest/api/test/article/repository/ArticleRepository.java
+++ src/main/java/com/example/rest/api/test/article/repository/ArticleRepository.java
@@ -1,7 +1,9 @@
 package com.example.rest.api.test.article.repository;
 
+import com.example.rest.api.test.article.entity.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ArticleRepository {
+public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
--- src/main/java/com/example/rest/api/test/article/service/ArticleService.java
+++ src/main/java/com/example/rest/api/test/article/service/ArticleService.java
@@ -1,7 +1,32 @@
 package com.example.rest.api.test.article.service;
 
+import com.example.rest.api.test.article.dto.ArticleDTO;
+import com.example.rest.api.test.article.entity.Article;
+import com.example.rest.api.test.article.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
 @Service
 public class ArticleService {
+    private final ArticleRepository articleRepository;
+    public List<ArticleDTO> getList() {
+        List<Article> articleList = this.articleRepository.findAll();
+
+        List<ArticleDTO> articleDTOList = articleList.stream()
+                .map(article -> new ArticleDTO(article))
+                .collect(Collectors.toList());
+
+        return articleDTOList;
+    }
+
+    public ArticleDTO getArticle(Long id) {
+        Optional<Article> optionalArticle = this.articleRepository.findById(id);
+
+        return optionalArticle.map(article -> new ArticleDTO(article)).orElse(null);
+    }
 }
